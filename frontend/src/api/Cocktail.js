5d685b87bde2ee96a55f5cfd8e64aed0f3da1d70
heeyun817 <vmffpfm79@naver.com>
2024-03-02 21:52:11 +0900

    feat: cocktails api 작업중

---
+++ frontend/src/api/Cocktail.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const getAllCocktails = async ({ sortBy = null } = {}) => {
+  try {
+    const response = await api.get('/cocktails', {
+      params: {
+        ...(sortBy !== null && { sort: sortBy })
+      },
+      withCredentials: true,
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching cocktails: ${error.message}`);
+  }
+};
+
--- frontend/src/pages/CocktailListPage.js
+++ frontend/src/pages/CocktailListPage.js
@@ -3,132 +3,39 @@ import styled from 'styled-components';
 import Header from '../components/common/Header';
 import Footer from '../components/common/Footer';
 import CocktailList from '../components/cocktail/CocktailList';
+import { getAllCocktails } from '../api/Cocktail';
 
 const CocktailListPage = () => {
 
   const [cocktails, setCocktails] = useState([]);
+  const [sortOption, setSortOption] = useState(null);
 
   useEffect(() => {
-    const mockCocktails = [
-      {
-        id: 1,
-        title: "test1",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 123,
-        likes: 20,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },{
-        id: 2,
-        title: "test2",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 0,
-        likes: 0,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },{
-        id: 3,
-        title: "test3",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 0,
-        likes: 0,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },{
-        id: 4,
-        title: "test4",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 0,
-        likes: 0,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },{
-        id: 5,
-        title: "test5",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 0,
-        likes: 0,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },{
-        id: 6,
-        title: "test5",
-        description: "설명입니당",
-        ingredient: "hihi",
-        recipe: "hihi",
-        abv: 2.5,
-        member: {
-          id: 1,
-          nickname: "heeyun"
-        },
-        createdAt: "2024-02-29T13:11:17.382313",
-        updatedAt: "2024-02-29T13:11:17.382313",
-        views: 0,
-        likes: 0,
-        images: [
-          "https://sw-team9.s3.ap-northeast-2.amazonaws.com/cook/6183b2b5-ddca-443b-81c3-22b529ed36ce.banner.jpeg"
-        ]
-      },
-      
-    ];
-    setCocktails(mockCocktails);
-  }, []);
+    const fetchData = async () => {
+      try {
+        let cocktailsData;
+        cocktailsData = await getAllCocktails({});
+
+          setCocktails(cocktailsData);
+      } catch (error) {
+        console.error('Error get cocktails:', error);
+      }
+    };
+
+    fetchData();
+  }, [sortOption]);
+
+  const handleSortClick = (sortOption) => {
+    setSortOption(sortOption);
+  };
 
   return (
     <>
       <Header />
       <BoardTitle>칵테일 레시피</BoardTitle>
-      <CocktailList cocktails={cocktails} />
+      <CocktailList 
+        cocktails={cocktails}
+        onSortClick={handleSortClick} />
       <Footer />
     </>
   );
